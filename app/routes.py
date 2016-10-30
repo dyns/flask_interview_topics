8713162c7d039a4b8c53c88e5c368cd37ade5994
dyns <dyns@users.noreply.github.com>
2016-10-29 23:01:12 -0600

    Update index

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 
-from flask import render_template, jsonify 
+from flask import render_template, jsonify, abort 
 from app import app
 from flask import request
 from app import models
@@ -8,11 +8,36 @@ from app import db
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	if request.method == 'GET':
-		top_sections = models.Section.query.all()
+		top_sections = models.Section.query.filter(~models.Section.sub_sections.any()).all()
 		sec_json = [s.json() for s in top_sections]
-		return render_template('index.html', sections=str(sec_json))
+		return render_template('index.html', sections=top_sections)
 	elif request.method == 'POST':
-		s = models.Section(request.form['title'])
-		db.session.add(s)
-		db.session.commit()
-		return jsonify(request.form)
+		title = request.form['title']
+		title = title.strip()
+		parent_id = request.form['parent-id']
+		parent = None
+		if parent_id:
+			try:
+				parent_id = int(parent_id)
+			except ValueError:
+				abort(400)
+			else:
+				parent = models.Section.query.get(parent_id)
+				if not parent:
+					abort(400)
+		if  title:
+			s = models.Section(title)
+			db.session.add(s)
+			if parent:
+				parent.sub_sections.append(s)			
+			db.session.commit()
+			return jsonify(request.form)
+		else:
+			abort(400)	
+
+
+@app.route('/s')
+def sections():
+	sections = models.Section.query.filter(~models.Section.sub_sections.any()).all()
+	return jsonify([s.json() for s in sections])
+
