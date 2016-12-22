d160f02520c23cd65db3d51955b6e35b011b22df
dyns <dyns@users.noreply.github.com>
2016-12-22 11:50:52 -0700

    Add confidence level

--- app/models.py
+++ app/models.py
@@ -7,7 +7,7 @@ class Section(db.Model):
 	title = db.Column(db.String(80), unique=True)
 	description = db.Column(db.Text)
 	due_date = db.Column(db.DateTime)
-	
+
 	parent_id = db.Column(db.Integer, db.ForeignKey('section.id'))
 	sub_sections = db.relationship('Section', backref=db.backref('parent', remote_side=[id]))
 
@@ -27,8 +27,8 @@ class Section(db.Model):
 		sub_section_json = [s.json() for s in self.sub_sections]
 		date = self.due_date.utcnow().strftime('%Y-%m-%d') if self.due_date else None
 		data = {
-			'title': self.title, 
-			'description':self.description, 
+			'title': self.title,
+			'description':self.description,
 			'due_date':date,
 			'sections': sub_section_json
 			}
@@ -40,14 +40,12 @@ class Task(db.Model):
 	description = db.Column(db.Text)
 	due_date = db.Column(db.DateTime)
 	parent_section_id = db.Column(db.Integer, db.ForeignKey('section.id'))
-	completed = db.Column(db.Boolean)
+	confidence = db.Column(db.Integer)
 
-	def __init__(self, title, description=None, due_date=None, completed = False, parent=None):
+	def __init__(self, title, description=None, due_date=None, confidence = 0, parent=None):
 		self.title = title
 		self.description = description
 		self.due_date = due_date
-		self.completed = completed
+		self.confidence = confidence
 		if parent:
 			self.parent = parent
-
-
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 
-from flask import render_template, jsonify, abort, url_for, redirect 
+from flask import render_template, jsonify, abort, url_for, redirect
 from app import app
 from flask import request
 from app import models
@@ -26,7 +26,7 @@ def index():
 			s = models.Section(title)
 			s.description = request.form['description']
 			if parent:
-				s.parent = parent	
+				s.parent = parent
 			db.session.add(s)
 			db.session.commit()
 			return redirect(url_for('index'))
@@ -40,16 +40,15 @@ def index():
 def add_progress(sections):
 	for section in sections:
 		set_section_progress(section)
-	
+
 
 def set_section_progress(section):
 	total_tasks = 0
 	completed_tasks = 0
 
-	total_tasks = len(section.tasks)
+	total_tasks = len(section.tasks) * 10
 	for task in section.tasks:
-		if task.completed:
-			completed_tasks += 1
+			completed_tasks += task.confidence
 
 	if section.sub_sections:
 		for sub_s in section.sub_sections:
@@ -61,48 +60,31 @@ def set_section_progress(section):
 	section.completed_tasks = completed_tasks
 
 	return (completed_tasks, total_tasks)
-		
-			
-		
 
 @app.route('/task', methods=['POST'])
 def task_post():
-	completed = False
-	if 'completed' in request.form:
-		completed = request.form['completed'] == '1'
-	
 	title = request.form['title'].strip()
 	description = request.form['description'].strip()
 	due_date = request.form['due-date']
 	due_date = datetime.strptime(due_date, '%Y-%m-%d')
 	parent_id = request.form['parent']
 	parent = models.Section.query.get(parent_id)
-	t = models.Task(title, description, due_date, completed, parent)
+	t = models.Task(title, description, due_date, 0, parent)
 	db.session.add(t)
 	db.session.commit()
 	return redirect(url_for('index'))
 
-@app.route('/task/<int:task_id>', methods=['POST'])
+@app.route('/task/<int:task_id>', methods=['GET','POST'])
 def update_task(task_id):
-	print('asdfasdf')
-	abort(404)
-
 	task = models.Task.query.get(task_id)
 	if task is None:
 		abort(404)
-	if 'completed' in request.form:
-		completed = request.form['completed'] == '1'
-		task.completed = completed
+	if 'confidence' in request.form:
+		task.confidence = int(request.form['confidence'])
 	db.session.commit()
 	return redirect(url_for('index'))
-	
 
 @app.route('/s')
 def sections():
 	sections = models.Section.query.all()
 	return jsonify([s.json() for s in sections])
-
-
-
-
-
