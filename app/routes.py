50dde0d99174245216155e69e926030a714981aa
dyns <dyns@users.noreply.github.com>
2016-12-24 16:39:45 -0700

    Add json loader

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
-
-from flask import render_template, jsonify, abort, url_for, redirect
+import os
+from flask import render_template, jsonify, abort, url_for, redirect, json
 from app import app
 from flask import request
 from app import models
@@ -144,3 +144,28 @@ def remove_children(sec):
 @app.route('/getting')
 def getting_interview():
 	return render_template('getting-the-interview.html')
+
+@app.route('/load')
+def load_default():
+	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+	json_url = os.path.join(SITE_ROOT, "static/", "def.json")
+	data = json.load(open(json_url))
+	for s in data['sections']:
+		db.session.add(add_json(s))
+	db.session.commit()
+	return redirect(url_for('index'))
+
+def add_json(section_json):
+	sub_sections = []
+	for s_s in section_json['sub-sections']:
+		sub_sections.append(add_json(s_s))
+
+	sec = models.Section(section_json['title'], section_json['description'], None, sub_sections)
+	# db.session.add(sec)
+
+	for t in section_json['tasks']:
+		description = t['description'] if 'description' in t else ''
+		task = models.Task(t['title'], description, None, 0, sec)
+		# db.session.add(task)
+
+	return sec
